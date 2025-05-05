4b440bd1034baf32469512bb9a957f749954900a
Wiqiro <william.imbert@utbm.fr>
2025-05-05 21:39:07 +0200

    feat: model & dataset services

--- app/datasets/page.tsx
+++ app/datasets/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { datasetsAtom } from "@/lib/atoms/datasetAtoms";
+import { useAtomValue } from "jotai";
+
 const Datasets = () => {
-    return <div>Datasets page</div>;
+    const datasets = useAtomValue(datasetsAtom);
+
+    return (
+        <div className="rounded-t-2xl bg-white shadow-sm px-10 w-full h-screen">
+            Datasets: {datasets.map((d) => d.name).join(", ")}
+        </div>
+    );
 };
 
 export default Datasets;
